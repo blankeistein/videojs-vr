9c4d7447197ae3b84751635559788cacdfb0cb2f
dev.blankeistein <dev.blankeistein@gmail.com>
2025-05-16 11:19:21 +0000

    fix videojs

--- src/VideoJS.jsx
+++ src/VideoJS.jsx
@@ -17,15 +17,13 @@ const VideoJS = (props) => {
         if (!playerRef.current) {
         // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode. 
         const videoElement = document.createElement("video-js");
-
+        videoElement.setAttribute("crossorigin", "anonymous");
         videoElement.classList.add('vjs-big-play-centered');
         videoRef.current.appendChild(videoElement);
 
         const player = playerRef.current = videojs(videoElement, options, () => {
             videojs.log('player is ready');
             onReady && onReady(player);
-
-            player.querySelector("video").setAttribute("crossorigin", "anonymous")
         });
 
         // You could update an existing player in the `else` block here
@@ -43,17 +41,16 @@ const VideoJS = (props) => {
         const player = playerRef.current;
 
         return () => {
-        if (player && !player.isDisposed()) {
-            player.dispose();
-            playerRef.current = null;
-            console.log("Test")
-        }
+            if (player && !player.isDisposed()) {
+                player.dispose();
+                playerRef.current = null;
+            }
         };
     }, [playerRef]);
 
     return (
         <div data-vjs-player>
-        <div ref={videoRef} crossorigin="anonymous" />
+            <div ref={videoRef} />
         </div>
     );
 }
